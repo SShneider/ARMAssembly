a00cdc30da65c7e24ff2b3296bbc72308b7ceb27
Sergei Shneider <shneider.s@gmail.com>
2020-08-27 05:24:15 -0400

    renaming regs

--- renamingregisters/main.s
+++ renamingregisters/main.s
@@ -1,1 +1,19 @@
 
+VAL1    .req        R1
+VAL2    .req        R2
+SUM     .req        R3
+
+                    .global main
+main:
+
+        MOV     VAL1,#0x25//#for literal
+        MOV     VAL2,#0x34
+
+        ADD     SUM, VAL1, VAL2
+
+        MOV     R7, #1
+        SVC     0
+
+        .end
+
+
